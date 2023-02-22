fa2f5f8167c559ee26558a25f75c950d0da3d0af
Guilherme Henrique <guilhermehenrique.fju@outlook.com>
2023-02-22 15:28:22 -0300

    Aula 13 De JavaScript

---
+++ aula13.js
@@ -0,0 +1,14 @@
+//console.log('Tudo bem?')
+
+
+var n1 = 1
+
+/*while (n1 < 3) {
+    console.log('Tudo bem?')
+    n1 += 1
+}*/
+
+do {
+    console.log('Tudo bem?')
+    n1 += 1
+} while (n1 < 3)

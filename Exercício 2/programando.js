51638c312a32f414fa46b1458cfe8c2acbf74e8f
Guilherme Henrique <guilhermehenrique.fju@outlook.com>
2023-02-22 13:46:33 -0300

    Atualização Exercício 2 JavaScript

--- Exercício 2/programando.js
+++
@@ -1,9 +0,0 @@
-function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.queroSelector('div#res')
-    if (fano.value.length == 0 || Number(fano.value) > ano) {
-        window.alert('[ERRO] Verifique os dados preenchidos e tente novamente')
-    } 
-}
